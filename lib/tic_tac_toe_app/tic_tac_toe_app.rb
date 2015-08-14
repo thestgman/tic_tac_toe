2dd7d9aac86776cc89afbe4606c151dae3d0711e
Andrei Stingaceanu <thestgman@gmail.com>
2015-08-14 20:04:18 +0100

    Make the game playable using a dumb computer AI which picks the first blank cell on the field.

--- lib/tic_tac_toe_app/tic_tac_toe_app.rb
+++ lib/tic_tac_toe_app/tic_tac_toe_app.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'io/console'
 
 module TicTacToeApp
 
@@ -7,8 +8,11 @@ module TicTacToeApp
     def initialize
       @field = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
       @cells = @field.each_with_index.map {|_, i| i}
-
-      # TODO: identify rows, columns, diagonals
+      @rows = @cells.each_slice(3).to_a
+      @columns = @rows.transpose
+      @diagonals = []
+      @diagonals << 3.times.map {|i| @cells[i * 4]}
+      @diagonals << 3.times.map {|i| @cells[2 + i * 2]}
     end
 
     # Returns all blank positions on the field.
@@ -23,6 +27,15 @@ module TicTacToeApp
     # Returns either the winning value from {'x', 'o'} if the game has
     # ended and it's not a draw or nil otherwise.
     def get_winner
+      # TODO: maybe store the winner in a var.
+
+      [@rows, @columns, @diagonals].each {|item|
+        item.each {|positions|
+          values = positions.map {|position| @field[position]}
+          return values[0] if (!values[0].nil? && values.uniq.length == 1)
+        }
+      }
+
       nil
     end
 
@@ -36,28 +49,48 @@ module TicTacToeApp
 
     # Inserts the play_symbol at position on the field
     def place_move(position, play_symbol)
-      nil
+      # TODO: validate position and play_symbol.
+
+      @field[position] = play_symbol
     end
 
     def to_s
-      "#{field[0..2].to_s}\n#{field[3..5].to_s}\n#{field[6..8].to_s}"
+      "#{@field[0..2].to_s}\n#{@field[3..5].to_s}\n#{@field[6..8].to_s}"
     end
   end
 
 
   # Encapsulates the human player logic.
   class Player
+    attr_reader :play_symbol
+
+    def initialize
+      @play_symbol = 'x'
+    end
+
     # Given the board, tick on the specified position.
-    def move
-      # TODO: implement.
+    def move(board, position)
+      board.place_move(position, @play_symbol)
     end
   end
 
 
   # Encapsulates the computer logic (AI?).
   class Computer
-    def move
-      # TODO: implement.
+    attr_reader :play_symbol
+
+    def initialize
+      @play_symbol = 'o'
+    end
+
+    def move(board)
+    	# TODO: smarten up Computer. The extremely dumb AI just picks the first blank position on the board.
+    	available_positions = board.blanks
+    	if available_positions.size > 0
+    		board.place_move(available_positions[0], @play_symbol)
+    	else
+    		throw 'Error: No more available positions. The game should have ended.'
+    	end
     end
   end
 
@@ -72,22 +105,51 @@ module TicTacToeApp
       @player = Player.new
       @cpu = Computer.new
       @winner = nil
+      @is_draw = false
     end
 
     def play
       until game_ended? do
-        # PLAY!
+        puts "\nYour move [0-8]:"
+        position = STDIN.getch
+        puts position
+        player_move(position)
+
+        unless game_ended?
+          puts "\nComputer move:"
+          cpu_move
+        end
       end
     end
 
     def player_move(position)
+      # TODO: find a better way to convert/validate here as "".to_i == 0
+      @player.move(@board, position.to_i)
     end
 
     def cpu_move
+      @cpu.move(@board)
+    end
+
+    def draw?
+      @is_draw
     end
 
     def game_ended?
-      true
+      show_board
+      if @board.draw?
+        @is_draw = true
+        puts "\nGame over! DRAW"
+        return true
+      elsif @board.game_over?
+        winner_value = @board.get_winner
+        @winner = (winner_value == @player.play_symbol ? 'player' : 'Computer')
+
+        puts "\nGame over! Winner: #{@winner}"
+        return true
+      end
+
+      false
     end
 
     def show_board
